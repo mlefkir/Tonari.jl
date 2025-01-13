153aab86acd8f521b29af0205f538ae1e2970170
Mehdy Lefkir <mehdylefkir.work@gmail.com>
2025-01-13 17:09:58 -0500

    fix typo in doc

--- src/periodogram.jl
+++ src/periodogram.jl
@@ -11,7 +11,7 @@ function end_matching(y, t)
 end
 
 @doc raw"""
-	periodogram(y, t, normalisation = "default"; apply_end_matching = false, subtract_mean = true)
+	periodogram(t,y, normalisation = "default"; apply_end_matching = false, subtract_mean = true)
 
 Compute the periodogram of the data y with time stamps t using the fast Fourier transform (FFT).
 
@@ -19,8 +19,8 @@ The periodogram is computed as the squared magnitude of the Fourier transform of
 use the real-valued fast Fourier transform (rfft) to compute the periodogram.
 
 # Arguments
-- `y::Array{Float64, 1}`: Time series data.
 - `t::Array{Float64, 1}`: Time stamps.
+- `y::Array{Float64, 1}`: Time series data.
 - `normalisation::Float64`: Normalisation factor. Default is 2Δt / length(t).
 - `apply_end_matching::Bool`: Apply end-matching to the data. Default is false.
 - `subtract_mean::Bool`: Subtract the mean from the data. Default is true.
